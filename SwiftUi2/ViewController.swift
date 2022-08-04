7e059dcbf4d216be75f2a8fe5ece0c13471e2c59
Kuvikinv <kuvikinvova@gmail.com>
2022-08-05 00:09:37 +0700

    Initial Commit

---
+++ SwiftUi2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUi2
+//
+//  Created by Владимир Кувикин on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
